1d661167498011b266159d33e18277aca49d3d5d
gitanshi-singh <gitanshi.college097@gmail.com>
2026-06-22 15:08:33 +0530

    27.05.2026

---
+++ Q1_shoppingcart.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace MAY_260526
+{
+    class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double GetTotal()
+        {
+            return Price * Quantity;
+        }
+    }
+    internal class Q1_shoppingcart
+    {
+        // create a shopping cart for multiple products. Show total cost of the shopping cart.
+        public static void Run()
+        {
+            Console.Write("Enter number of products: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Product[] cart = new Product[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                cart[i] = new Product();
+
+                Console.WriteLine($"\nProduct {i + 1}");
+
+                Console.Write("ID: ");
+                cart[i].Id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Name: ");
+                cart[i].Name = Console.ReadLine();
+
+                Console.Write("Price: ");
+                cart[i].Price=Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Quantity: ");
+                cart[i].Quantity = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double grandTotal = 0;
+
+            Console.WriteLine("\nShopping Cart");
+            Console.WriteLine("-----------------------------");
+
+            foreach (Product p in cart)
+            {
+                double total = p.GetTotal();
+
+                Console.WriteLine(
+                    $"ID:{p.Id} Name:{p.Name} Price:{p.Price} Qty:{p.Quantity} Total:{total}");
+
+                grandTotal+= total;
+            }
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Grand Total = " + grandTotal);
+        }
+    }
+}
---
+++ Q2_employeeHierarchy.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MAY_260526
+{
+    class Employee
+    {
+        public string Name;
+
+        public List<Employee> Subordinates =
+            new List<Employee>();
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public void AddSubordinate(Employee emp)
+        {
+            Subordinates.Add(emp);
+        }
+        public void Display(int level)
+        {
+            for (int i = 0; i < level; i++)
+            {
+                Console.Write("   ");
+            }
+
+            Console.WriteLine(Name);
+
+            foreach (Employee emp in Subordinates)
+            {
+                emp.Display(level + 1);
+            }
+        }
+    }
+    internal class Q2_employeeHierarchy
+    {
+//create tree structure (ceo, manager, subordinates)
+    public static void Run()
+        {
+            Employee ceo = new Employee("CEO");
+
+            Employee manager1 = new Employee("Manager A");
+            Employee manager2 = new Employee("Manager B");
+
+            Employee emp1 = new Employee("Employee 1");
+            Employee emp2 = new Employee("Employee 2");
+            Employee emp3 = new Employee("Employee 3");
+
+            manager1.AddSubordinate(emp1);
+            manager1.AddSubordinate(emp2);
+
+            manager2.AddSubordinate(emp3);
+
+            ceo.AddSubordinate(manager1);
+            ceo.AddSubordinate(manager2);
+
+            Console.WriteLine("Company Hierarchy:\n");
+
+            ceo.Display(0);
+
+        }
+    }
+}
